b8e1e39a310211a12888feccd2eb91945434ec6d
Angel <angel@Angels-MacBook-Air.local>
2023-11-17 11:53:58 -0500

    updated seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -10,19 +10,23 @@ VALUES
 INSERT INTO 
     topics(lessons_id, name, description) 
 VALUES
-    (1, 'Octaves','The octave is a fundamental musical interval that spans eight diatonic notes or twelve semitones.'),
+    (1, 'Notes on the Piano and Octaves','◦ There are 8 notes in music, ABCDEFG.
+    ◦ The notes on the piano are ABCDEFG.
+    ◦ This sequence of notes repeats. Example: ABCDEFGABCDEFG
+    ◦ Every repetition is called an octave.'),
 
-    (1, 'Notes on the Piano','The piano utilizes 88 keys spanning over seven octaves, each key corresponding to a specific musical pitch.'),
+    (1, 'Intervals and Chords','◦ The relationship between two notes is called an interval.
+    ◦ These intervals have various names and qualities (a minor third, a major sixth, a perfect fifth).
+    ◦ At least three notes played simultaneously is called a chord.
+    ◦ Chords also have qualities, major and minor the most common.'),
 
-    (1, 'Intervals','An interval in music refers to the distance between two pitches.'),
+    (2, 'Bars and Types of Notes','◦ A bar is a way of understanding time in music. All music is played in terms of bars.
+    ◦ Each bar has a certain amount of beats. Usually, in most music, a bar has 4 beats, each being a quarter note.
+    ◦ The most common types of notes are whole notes, half notes, quarter notes, eighth notes, and sixteenth notes.
+    ◦ Each of these has a certain beat value, which quarter notes the majority of the time representing 1 beat.'),
 
-    (1, 'Chords','Chords are formed by playing three or more notes together.'),
-
-    (2, 'Bars','The bar is the basic unit of time in music. It defines the periodic division of rhythm'),
-
-    (2, 'Types of Notes','Notes represent different durations of sounds.'),
-
-    (2, 'Tempo','Tempo is how many beats per minute are being played, so how fast the song is.');
+    (2, 'Tempo','◦ Tempo is how many beats per minute are being played, so how fast the song is.
+    ◦ Tempo is very important to the feel of a song, and generally informs how a song should be played and constructed.');
 
 
 -- INSERT INTO 
