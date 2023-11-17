8d574e00ae2bfc337da94d6ad04906176a0b4784
thandi bristol <thandibristol@thandis-air.mynetworksettings.com>
2023-11-17 11:12:53 -0500

    combined schema/seed.sgl

--- db/sseed.sql
+++
@@ -1,7 +0,0 @@
-\c wavform_dev;
-
-INSERT INTO 
-    users(username, password, email)
-VALUES
-    ('stormLord', 'password321', 'wave1@hotmail.com'),
-    ('boogieWOnderland', 'earthwindandfire', 'form2@hotmail.com');
