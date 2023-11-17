2152183f839263247cd64aa81d26357ab6224835
ambarimar ulloa <ambarimar@ambarimars-Air.fios-router.home>
2023-11-17 12:33:38 -0500

    about backend

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
-const lessonsController = require("./controllers/lessonsController");
-const topicsController = require("./controllers/topicsController");
+ const lessonsController = require("./controllers/lessonsController");
+ const topicsController = require("./controllers/topicsController");
+const aboutController = require("./controllers/aboutController");
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use("/lessons", lessonsController);
-app.use("/topics", topicsController);
+ app.use("/lessons", lessonsController);
+ app.use("/topics", topicsController);
+ app.use("/about", aboutController);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Wavform App");
---
+++ controllers/aboutController.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+const { getAllCreators, getSingleCreator } = require("../queries/about");
+
+router.get("/", async (req, res) => {
+  const allPeople = await getAllCreators();
+
+  console.log(allPeople);
+
+     if (!Array.isArray(allPeople)){
+        res.status(500).json({error:"server error"});
+    } else {
+        res.json(allPeople);
+    }
+ 
+});
+
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    const person = await getSingleCreator();
+    if (person.length === 0 ){
+        res.status(404).json({error: "Not found"})
+    }else {
+        res.json(people[0]);
+    }
+});
+
+
+module.exports = router;
