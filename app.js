1ac49e552c63b8f36eb89c1cb9a1260be5262d03
thandi bristol <thandibristol@thandis-air.mynetworksettings.com>
2023-11-28 12:42:08 -0500

    updated user backend

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
  const lessonsController = require("./controllers/lessonsController");
  const topicsController = require("./controllers/topicsController");
 const aboutController = require("./controllers/aboutController");
+const lessonsController = require("./controllers/lessonsController");
+const topicsController = require("./controllers/topicsController");
+const UserRouter = require("./routers/userRouter")
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +17,10 @@ app.use(morgan("dev"));
  app.use("/lessons", lessonsController);
  app.use("/topics", topicsController);
  app.use("/about", aboutController);
+
+app.use("/lessons", lessonsController);
+app.use("/topics", topicsController);
+app.use("/users", UserRouter)
 
 app.get("/", (req, res) => {
   res.send("Welcome to Wavform App");
